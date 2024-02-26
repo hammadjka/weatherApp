6103194d13822ee6046ed6b9156817f74ded0a55
Hammad <javedhammad034@gmail.com>
2024-02-26 02:10:32 -0800

    added styling, displaying of info, project completed

--- src/index.js
+++ src/index.js
@@ -34,6 +34,10 @@ async function getWeather(locationQuery){
     })
     try {
         const response = await fetch(`${baseUrl}${endpoint}?${params}`);
+        if (!response.ok) {
+            alert("No results found");
+            return;
+        }
         const data = await response.json();
         return data;
     } catch (error) {
@@ -42,7 +46,50 @@ async function getWeather(locationQuery){
     }
 }
 
+function showWeather(weather){
+    let city = document.getElementById("city");
+    let distAndCountry = document.getElementById("distCountry");
+    let time = document.getElementById("time");
+    let currentLocation = weather["location"];
+
+    city.textContent = currentLocation["name"];
+    distAndCountry.textContent = "(" +  currentLocation["region"] + ", " + currentLocation["country"] + ")";
+    time.textContent = "Local time: " + currentLocation["localtime"];
+
+    let temp = document.getElementById("temperature");
+    let cond = document.getElementById("conditions");
+    let press = document.getElementById("pressure");
+    let humidity = document.getElementById("humidity");
+    let precip = document.getElementById("precipitation");
+    let wind = document.getElementById("wind");
+    let currentWeather = weather["current"];
+
+    temp.textContent = currentWeather["temp_c"] + "°C / Feels like " + currentWeather["feelslike_c"] + "°C";
+    press.textContent = currentWeather["pressure_in"] + " in";
+    cond.textContent = currentWeather["condition"]["text"];
+    document.getElementById("conditionIcon").setAttribute('src', currentWeather["condition"]["icon"] )
+    humidity.textContent = currentWeather["humidity"] + "%";
+    precip.textContent = currentWeather["precip_in"] + " in";
+    wind.textContent = currentWeather["wind_dir"] + " @ " + currentWeather["wind_degree"] + "°, " + currentWeather["wind_kph"] + " kph";
+    
+    const isDay = currentWeather["is_day"];
+    if(isDay){
+        document.body.style.animation = 'toDay 3s forwards';
+    }else{
+        document.body.style.animation = 'toNight 3s forwards';
+    }
+
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
+
+    try{
+        const defaultCity = await getWeather("Vancouver");
+        showWeather(defaultCity);
+        console.log(defaultCity);
+    }catch(error){
+        console.log(error);
+    }
 
     document.getElementById("search").addEventListener("input", async (event)=>{
         try{
@@ -50,9 +97,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         }catch(error){
             console.log(error);
         }
-        
     });
-
     document.querySelector("#form").addEventListener("submit", async (e)=>{
         e.preventDefault();
         const formData = new FormData(form);
@@ -60,12 +105,14 @@ document.addEventListener("DOMContentLoaded", async () => {
         for (const [key, value] of formData) {
             formDataObject[key] = value;
         }
-        const query = formDataObject["search"];
+        const query = formDataObject["inputSearch"];
+        let weather;
         try{
-            const weather = await getWeather(query);
+            weather = await getWeather(query);
             console.log(weather);
         }catch(error){
             console.log(error);
         }
+        showWeather(weather);
     })
 })
